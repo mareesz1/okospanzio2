91f47b8633515fd5f891e9f907f417d7445c6a62
csanda7 <csandanorbert@gmail.com>
2023-03-09 08:37:46 +0100

    rooms
    roomview

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import {useUsersStore} from '../stores/index';
+import {piniaStore} from '../main';
 import HomeView from '../views/HomeView.vue'
+import RoomView from '../views/RoomsView.vue'
 import LoginView from '../views/LoginView.vue';
 import NewUserView from '../views/RegistrationView.vue';
-import AdminView from '../views/AdminView.vue';
+import AdminView from '../views/admin/AdminView.vue';
+import AUsersTableView from '../views/admin/AdminUsersTableView.vue';
+import ARoomsView from '../views/admin/AdminRoomsView.vue';
+// import {storeToRefs} from 'pinia';
+
+// const valtozo = useUsersStore();
+
+// console.log(useUsersStore.state);
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,27 +21,63 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {title: 'Home'},
+    },
+    {
+      path: '/rooms',
+      name: 'room',
+      component: RoomView,
+      meta: {title: 'Rooms'},
     },
     {
       path: '/login',
       name: 'login',
       component: LoginView,
+      meta: {title: 'Login'},
     },
-    // {
-    //   path: '/post',
-    //   redirect: '/login',
-    // },
     {
-      path: '/newuser',
-      name: 'newuser',
+      path: '/register',
+      name: 'register',
       component: NewUserView,
+      meta: {title: 'Register'},
     },
     {
       path: '/admin',
       name: 'admin',
       component: AdminView,
+      meta: {title: 'Admin'},
+      beforeEnter: (to, from) => {
+        const loginData = JSON.parse(localStorage.getItem("login"));
+        if (loginData.roles !== "admin" || !loginData.auth) {
+          return false;
+        } else {
+          // 
+        }
+      },
+      children: [
+        {
+          path: 'users',
+          component: AUsersTableView,
+        },
+        {
+          path: 'rooms',
+          component: ARoomsView,
+        },
+      ]
     },
-  ]
-})
-
-export default router
+    // {
+    //   path: '/admin/modifyuser/:id',
+    //   name: 'modifyUser',
+    //   component: ModifyUser,
+    //   meta: {title: 'Modify User'},
+    //   beforeEnter: (to, from) => {
+    //     const loginData = JSON.parse(localStorage.getItem("login"));
+    //     if (loginData.roles !== "admin" || !loginData.auth) {
+    //       return false;
+    //     } else {
+    //       // 
+    //     }
+    //   }
+    // },
+]});
+export default router;
---
+++ frontend/src/views/RoomsView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p class="display-1 text-center mb-3">Rooms</p>
+        <navbar/>
+    </div>
+    <div class="container my-3">
+        <room-component/>
+    </div>
+</template>
+
+<script setup>
+import navbar from '../components/NavbarComponent.vue';
+import RoomComponent from '../components/RoomComponent.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
