23945a84809e6c94328052f40a60ff0de92a2df7
csanda7 <csandanorbert@gmail.com>
2023-03-10 12:01:49 +0100

    button - navbar animation

--- frontend/src/components/RegisterButtonComponent.vue
+++ frontend/src/components/RegisterButtonComponent.vue
@@ -1,12 +1,12 @@
 <template>
-      <div class="wrapper">
+      <div class="wrapper align-center">
         <div class="link_wrapper">
-          <button>Register</button>
-            <div class="icon">
+          <a>Register</a>
+            <!-- <div class="icon">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
               <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
               </svg>
-            </div>
+            </div> -->
         </div>
       </div>
 </template>
@@ -15,70 +15,26 @@
 </script>
 
 <style lang="css" scoped>
-    body{
-  font-family: 'Lato', sans-serif;
-}
-
 .wrapper{
-  position: fixed;
-  top: 46%;
-  left: 75%;
-  transform: translate(-50%, -50%);
-  
-}
-
-.link_wrapper{
-  position: relative;
-}
-
-button{
-  display: block;
-  width: 250px;
-  height: 50px;
-  /* line-height: 50px; */
-  font-weight: bold;
-  text-decoration: none;
-  background: #333;
   text-align: center;
-  color: #fff;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  border: 3px solid #333;
-  transition: all .35s;
-  /* padding-bottom: 52px; */
 }
+.link_wrapper {
+color: #494949 !important;
+text-transform: uppercase;
+background: #ffffff;
+padding: 20px;
+border: 4px solid #494949 !important;
+border-radius: 6px;
+display: inline-block;
+width: fit-content;
+text-align: center;
+transition: all 0.3s ease 0s;
 
-.icon{
-  width: 50px;
-  height: 50px;
-  border: 3px solid transparent;
-  position: absolute;
-  transform: rotate(45deg);
-  right: 0;
-  top: 0;
-  z-index: -1;
-  transition: all .35s;
 }
-
-.icon svg{
-  width: 30px;
-  position: absolute;
-  top: calc(50% - 15px);
-  left: calc(50% - 15px);
-  transform: rotate(-45deg);
-  fill: #2ecc71;
-  transition: all .35s;
-}
-
-button:hover{
-  width: 200px;
-  border: 3px solid #2ecc71;
-  background: transparent;
-  color: #2ecc71;
-}
-
-button:hover + .icon{
-  border: 3px solid #2ecc71;
-  right: -25%;
+.link_wrapper:hover {
+color: #20bf6b !important;
+border-radius: 50px;
+border-color: #20bf6b !important;
+transition: all 0.3s ease 0s;
 }
 </style>
